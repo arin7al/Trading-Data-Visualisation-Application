4b98e4a86c9c6b0bc4b3dae0d9419a5b6ba79121
arin7al <smesh_arina@mail.ru>
2020-08-21 09:56:10 +0100

    added initial files

---
+++ Instrument.py
@@ -0,0 +1,7 @@
+def calculateNextPrice(self, direction):
+    newPriceStarter = self.__price + numpy.random.normal(0, 1) * self.__variance + self.__drift
+    newPrice = newPriceStarter if (newPriceStarter > 0) else 0.0
+    if self.__price < self.__startingPrice * 0.4:
+        self.__drift = (-0.7 * self.__drift)
+    self.__price = newPrice * 1.01 if direction == 'B' else newPrice * 0.99
+    return self.__price
