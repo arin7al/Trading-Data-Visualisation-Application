289ad8c0daa3a7355fb0b50ad514c3663fe857a7
arin7al <smesh_arina@mail.ru>
2020-08-21 10:07:44 +0100

    added Instrument.py and initialRandomValues

---
+++ business-logic/Instrument.py
@@ -0,0 +1,20 @@
+import numpy, random
+
+
+class Instrument:
+    def __init__(self, name, variance, drift, startingPrice, price):
+        self.data = []
+        self.name = name
+        self.__variance = variance
+        self.__drift = drift
+
+        self.__price = price
+        self.__startingPrice = startingPrice
+
+    def calculateNextPrice(self, direction):
+        newPriceStarter = self.__price + numpy.random.normal(0, 1) * self.__variance + self.__drift
+        newPrice = newPriceStarter if (newPriceStarter > 0) else 0.0
+        if self.__price < self.__startingPrice * 0.4:
+            self.__drift = (-0.7 * self.__drift)
+        self.__price = newPrice * 1.01 if direction == 'B' else newPrice * 0.99
+        return self.__price
